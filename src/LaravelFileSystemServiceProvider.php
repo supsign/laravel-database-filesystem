9731f2d18c018786ef50d882de80a0b152b409d3
Florian Ratz <florian.ratz@supsign.ch>
2021-02-17 15:28:53 +0100

    service provider

---
+++ src/LaravelFileSystemServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Supsign\LaravelFileSystem;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelFileSystemServiceProvider extends ServiceProvider
+{
+
+    public function boot() 
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
